de7dd31bd3db8d130f60345e823925dba641047d
Allie LeFever <alefever39@gmail.com>
2022-08-09 13:16:40 -0600

    added user auth, error handling

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,37 @@
 class UsersController < ApplicationController
+  skip_before_action :authorize, only: %i[show create]
+
+  ############################### /users
   def index
     render json: User.all
+  end
+
+  ############################### /me
+  def show
+    user = User.find_by!(id: session[:user_id])
+    render json: user
+  end
+
+  ############################### /signup
+  def create
+    if params[:password_confirmation] == params[:password]
+      user = User.create!(params_hash)
+      session[:user_id] = user.id
+      render json: user, status: :created
+    else
+      render json: { errors: ["Confirmation password does not match password"] }
+    end
+  end
+
+  private
+
+  def user_params
+    params.permit(
+      :email,
+      :password,
+      :password_confirmation,
+      :first_name,
+      :last_name
+    )
   end
 end
